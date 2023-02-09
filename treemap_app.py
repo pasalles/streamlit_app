af66b8efff9829db47077504a4d16917b45d28fc
pasalles <114933430+pasalles@users.noreply.github.com>
2023-02-09 19:27:23 -0300

    Add files via upload

--- treemap_app.py
+++ treemap_app.py
@@ -12,6 +12,9 @@ credentials = service_account.Credentials.from_service_account_file(
 project_id = 'geometric-gamma-370017'
 client = bigquery.Client(credentials= credentials,project=project_id)
 
+#Title
+st.title('Portfolio stock performance')
+
 # Time intervals for the SQL query
 
 time_intervals = ['1 day', '1 week', '1 month']
@@ -61,10 +64,11 @@ query_job = client.query(query)
 df = query_job.to_dataframe()
 
 # Create the Treemap chart
+labels = {'price_difference': 'change'}
 fig = px.treemap(df, path=['sector', 'industry', 'stock_code'], values='market_cap',
-                  color='price_difference': 'change', hover_data=['price_difference'], 
+                  color='price_difference', hover_data=['price_difference'], 
                   color_continuous_scale=[(0, "#f63538"), (0.5, "#414554"), (1, "#30cc5a")],
-                  color_continuous_midpoint=0, range_color=[-3,3])
+                  color_continuous_midpoint=0, range_color=[-3,3], labels=labels)
 
 fig.data[0].customdata = fig.data[0].marker.colors
 fig.data[0].texttemplate = "<b>%{label}</b><br>Market Cap: %{value:.0f} B<br>change: %{customdata:.2f}%<br>"
@@ -76,5 +80,5 @@ fig.update_layout({
 })
 fig.update_layout(margin = dict(t=10, l=10, r=10, b=10), autosize=True, width=1000)
 
-st.title('Portfolio stock performance')
+
 st.plotly_chart(fig, use_container_width=True)
